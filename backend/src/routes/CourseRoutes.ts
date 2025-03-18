6b3818447f7c26a537264152f5e20735cdfd3ba3
Rafael Leite <rafanleite@gmail.com>
2025-03-18 15:47:11 -0300

    refactor: remove JavaScript files in favor of Java implementation

--- backend/src/routes/CourseRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import CourseController from "../controllers/CourseController";
-
-const router = Router();
-
-router.get("/", CourseController.getCourses);
-router.post("/", CourseController.createCourse);
-
-export default router;
