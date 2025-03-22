54c267f333cbcd12ca6a202e26eac168a026217e
Rafael Leite <rafanleite@gmail.com>
2025-03-22 18:21:26 -0300

    feat: add Discipline entity and repository

---
+++ backend/src/main/java/com/roadmap4it/domain/entity/Discipline.java
@@ -0,0 +1,21 @@
+package com.roadmap4it.domain.entity;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Discipline {
+    private String code;
+    private String name;
+    private String description;
+    private int semester;
+    private List<String> prerequisites;
+    private List<String> unlocks;
+}
--- backend/src/main/java/com/roadmap4it/domain/repository/DisciplineRepository.java
+++ backend/src/main/java/com/roadmap4it/domain/repository/DisciplineRepository.java
@@ -1,5 +1,22 @@
 package com.roadmap4it.domain.repository;
 
-public class DisciplineRepository {
-    
+import com.roadmap4it.domain.entity.Discipline;
+import java.util.List;
+import java.util.Optional;
+
+public interface DisciplineRepository {
+
+    Optional<Discipline> findByCode(String code);
+
+    List<Discipline> findBySemester(int semester);
+
+    List<Discipline> findAllDisciplines();
+
+    Discipline saveDiscipline(Discipline discipline);
+
+    // void delete(Discipline discipline);
+
+    void deleteDisciplineById(Long id);
+
+    boolean existsByCode(String code);
 }
